8f2af18fd4b8637c3d054d34df616ade2482ec8b
4468 <446853122@qq.com>
2016-04-17 00:31:21 +0800

    417031

--- app/src/main/java/com/jiai/sun/appforenglist/Activity/LearnEnglishActivity.java
+++ app/src/main/java/com/jiai/sun/appforenglist/Activity/LearnEnglishActivity.java
@@ -2,14 +2,16 @@ package com.jiai.sun.appforenglist.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.jiai.sun.appforenglist.R;
 
 public class LearnEnglishActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_english);
+
     }
 }
